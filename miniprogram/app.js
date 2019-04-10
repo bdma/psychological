df9e47b79886fc11cc125d26ebf1013a9d0cc775
zhangqinghe <zhangqinghe@pyyx.com>
2019-04-10 18:36:37 +0800

    update list css

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -11,5 +11,20 @@ App({
     }
 
     this.globalData = {}
-  }
+
+    wx.getSetting({
+      success(res) {
+        console.log(res)
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success(res) {
+              console.log(res.userInfo)
+            }
+          })
+        }
+      }
+    })
+  },
+
 })
